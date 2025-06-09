931a46f507522a5449b8b2681d8d8b27b4f28437
Astrid <me@astr.cc>
2025-06-09 22:57:26 +0200

    yeet the python code

--- cashews/scripts/list.py
+++
@@ -1,8 +0,0 @@
-from cashews import utils, DATA_DIR
-
-nouns = utils.get_object("nouns", "nouns")
-adjectives = utils.get_object("adjectives", "adjectives")
-with open(f"{DATA_DIR}/nouns.txt", "w") as f:
-    f.write("\n".join(nouns))
-with open(f"{DATA_DIR}/adjectives.txt", "w") as f:
-    f.write("\n".join(adjectives))
--- cashews/scripts/updategames.py
+++
@@ -1,6 +0,0 @@
-from cashews import utils
-import tqdm
-
-utils.init_db()
-for game_id in tqdm.tqdm(utils.get_all_ids("game")):
-    utils.update_game_data(game_id)
