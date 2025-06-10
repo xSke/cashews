b6d070e9d134df9e1f4ad8cd1889cef18a5d9510
Astrid <me@astr.cc>
2025-06-10 23:41:17 +0200

    change color schjeme aghain

--- web/app/lib/colors.ts
+++ web/app/lib/colors.ts
@@ -1,11 +1,12 @@
 import chroma from "chroma-js";
 import colors from "tailwindcss/colors";
+
 export const lightScale = chroma
-  .scale([colors.orange[700], colors.gray[700], colors.blue[700]])
+  .scale([colors.blue[500], colors.neutral[600], colors.red[500]])
   .domain([0, 0.5, 1])
   .mode("lab");
 
 export const darkScale = chroma
-  .scale([colors.orange[500], colors.gray[200], colors.blue[500]])
+  .scale([colors.blue[500], colors.neutral[200], colors.red[500]])
   .domain([0, 0.5, 1])
   .mode("lab");
