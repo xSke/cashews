b9188984592338ff75b4c07cb210e2a8025e9776
Robby Blum <20372070+robbyblum@users.noreply.github.com>
2025-06-10 20:47:34 -0400

    make the page title not "Tanstack React Router" lol

--- web/app/routes/__root.tsx
+++ web/app/routes/__root.tsx
@@ -27,7 +27,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "Free Cashews",
       },
     ],
     links: [
