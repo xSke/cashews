440eb19deafe7fe3fabf777c73a6bc63fa2c2935
Astrid <me@astr.cc>
2025-06-26 19:41:08 +0200

    more sql

--- chron-db/migrations/views.sql
+++ chron-db/migrations/views.sql
@@ -1,11 +1,128 @@
+-- lol
+CREATE OR REPLACE FUNCTION println(str text) RETURNS integer
+    LANGUAGE plpgsql AS
+$$BEGIN
+    RAISE NOTICE '%', str;
+    RETURN 42;
+END;$$;
+
 drop materialized view if exists game_player_stats_exploded cascade;
 drop materialized view if exists game_player_stats_league_aggregate cascade;
 drop materialized view if exists game_player_stats_global_aggregate cascade;
 drop materialized view if exists pitches cascade;
- 
+
 drop view if exists league_percentiles;
 drop view if exists game_player_stats_advanced;
 
+select println('creating players');
+drop materialized view if exists players cascade;
+create materialized view players as
+    select
+        lv.entity_id as player_id,
+        (jt.first_name || ' ' || jt.last_name) as full_name,
+        jt.* from latest_versions lv
+    inner join objects using (hash)
+    join lateral json_table(objects.data, '$[*]' columns (
+        first_name text PATH '$.FirstName',
+        last_name text PATH '$.LastName',
+        team_id text PATH '$.TeamID',
+        throws text PATH '$.Throws',
+        bats text PATH '$.Bats',
+        position text PATH '$.Position',
+        position_type text PATH '$.PositionType',
+        likes text PATH '$.Likes',
+        dislikes text PATH '$.Dislikes',
+        durability real PATH '$.Durability',
+        augments smallint PATH '$.Augments',
+        number smallint PATH '$.Number'
+    )) jt on true
+    where lv.kind = 4;
+create unique index players_pkey_idx on players(player_id);
+
+select println('creating team feeds');
+drop materialized view if exists team_feeds cascade;
+create materialized view team_feeds as
+    select
+        lv.entity_id as team_id,
+        jt.* from latest_versions lv
+    inner join objects using (hash)
+    join lateral json_table(objects.data, '$.Feed[*]' columns (
+        season smallint PATH '$.season',
+        day smallint PATH '$.day',
+        text text PATH '$.text',
+        type text PATH '$.type',
+        emoji text PATH '$.emoji',
+        timestamp timestamptz PATH '$.ts',
+        player_ids TEXT[] PATH '($.links[*] ? (@.type == "player")).id' WITH ARRAY WRAPPER,
+        team_ids TEXT[] PATH '($.links[*] ? (@.type == "team")).id' WITH ARRAY WRAPPER,
+        game_ids TEXT[] PATH '($.links[*] ? (@.type == "game")).id' WITH ARRAY WRAPPER
+    )) jt on true
+    where lv.kind = 3;
+create unique index team_feeds_pkey_idx on team_feeds(team_id, timestamp);
+
+select println('creating rosters');
+drop materialized view if exists rosters cascade;
+create materialized view rosters as
+    select lv.entity_id as team_id, jt.* from latest_versions lv
+    inner join objects using (hash)
+    join lateral json_table(objects.data, '$.Players[*] ? (@.PlayerID != "#")' columns (
+        player_id text PATH '$.PlayerID',
+        slot_index for ordinality,
+        slot text PATH '$.Slot'
+    )) jt on true
+    where lv.kind = 3;
+create unique index rosters_pkey_idx on rosters(team_id, slot);
+
+select println('creating roster slots');
+drop materialized view if exists roster_slots;
+create materialized view roster_slots as
+    with
+        roster_slot_versions as (
+            select
+                v.entity_id as team_id,
+                jt.slot,
+                jt.index,
+                jt.player_id,
+                v.valid_from,
+                v.valid_to
+            from versions v
+                     inner join objects using (hash)
+                     join lateral json_table(objects.data, '$.Players[*]' columns (
+                index for ordinality,
+                player_id text PATH '$.PlayerID',
+                slot text PATH '$.Slot'
+            )) jt on true
+            where v.kind = 10
+        ), slots_with_increment as (
+        select
+            *,
+            case
+                when (lag(player_id) over w) is distinct from player_id then 1
+                end as version_increment
+        from roster_slot_versions
+        window w as (partition by team_id, slot order by valid_from rows between unbounded preceding and current row)
+    ), slots_with_seq as (
+        select
+            *,
+            count(version_increment) over w as seq
+        from slots_with_increment
+        window w as (partition by team_id, slot order by valid_from rows between unbounded preceding and current row)
+    )
+    select
+        team_id,
+        slot,
+        seq,
+        any_value(index) as slot_index,
+        any_value(player_id) as player_id,
+        min(valid_from) as valid_from,
+        -- todo: should this contain nulls? how to make max do that right?
+        max(coalesce(valid_to, 'infinity')) as valid_to
+    from slots_with_seq
+    group by team_id, slot, seq;
+create unique index roster_slots_pkey_idx on roster_slots(team_id, slot, seq);
+create index roster_slots_player_idx on roster_slots(player_id, valid_from, valid_to);
+
+select println('creating game_player_stats_exploded');
 create materialized view game_player_stats_exploded as
     select gps.season, gps.day, gps.game_id, gps.player_id, gps.team_id, jt.* from game_player_stats gps, json_table(data, '$[*]' columns (
         allowed_stolen_bases smallint PATH '$.allowed_stolen_bases' default 0 on empty default 0 on error,
@@ -67,6 +184,8 @@ create materialized view game_player_stats_exploded as
         wins smallint PATH '$.wins' default 0 on empty default 0 on error
     )) as jt;
 create unique index game_player_stats_exploded_pkey on game_player_stats_exploded(game_id, team_id, player_id);
+
+select println('creating game_player_stats_exploded indexes');
 create index game_player_stats_exploded_by_season_day_idx on game_player_stats_exploded(season, day);
 create index game_player_stats_exploded_by_player_idx on game_player_stats_exploded(player_id, season, day);
 create index game_player_stats_exploded_by_team_idx on game_player_stats_exploded(team_id, season, day);
@@ -238,7 +357,8 @@ group by (season, h.player_id, h.team_id);
 -- Materialized view for calculating league-average stats, for use in normalized statistics and the like. Averages over
 -- individual leagues, so there will be a bit of variation in how OPS translates to OPS+ for different Lesser Leagues,
 -- if you use this. Join this to the team info on league_id to select the relevant league average.
--- particular note: FIP constant is implicitly defined in here: use fip_const = era - fip_base to get it
+-- particular note: FIP constant is implicitly defined in here: use fip_const = era - fip_base to get it\
+select println('creating game_player_stats_league_aggregate');
 create materialized view if not exists game_player_stats_league_aggregate as
 with helper as (
     select
@@ -386,6 +506,7 @@ create unique index game_player_stats_league_aggregate_idx on game_player_stats_
 -- by league, so BA here is (total hits across every league in MMOLB) / (total at-bats across every league in MMOLB) for
 -- a given season.
 -- particular note: FIP constant is implicitly defined in here: use fip_const = era - fip_base to get it
+select println('creating game_player_stats_global_aggregate');
 create materialized view if not exists game_player_stats_global_aggregate as
 with helper as (
     select
@@ -736,12 +857,14 @@ group by (season);
 $$
     language sql;
 
+select println('creating pitches');
 create materialized view if not exists pitches as
-    select 
+    select
         *,
         split_part(data->>'pitch_info', ' ', 1)::real as pitch_speed,
         split_part(data->>'pitch_info', 'MPH ', 2) as pitch_type,
         nullif(data->>'zone', '')::smallint as pitch_zone
     from game_events
         where pitcher_id is distinct from null and data->>'event' = 'Pitch';
+select println('creating pitches index');
 create unique index pitches_idx on pitches(game_id, index);
