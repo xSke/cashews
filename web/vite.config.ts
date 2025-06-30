535ce2a4b9771f28a2ce2c2873bca8324726bafe
Astrid <me@astr.cc>
2025-06-30 22:21:11 +0200

    upgrade tanstack start

---
+++ web/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, loadEnv } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // change this in `.env`
+  const API_BASE = env.API_BASE ?? "https://freecashe.ws/api";
+
+  return {
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: API_BASE,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    plugins: [tsConfigPaths(), tanstackStart()],
+  };
+});
